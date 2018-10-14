ab064426210919750d703951c68b1431b3b6ea0e
Dmitry Fomin <fomin.dm@gmail.com>
2018-10-15 00:39:35 +0300

    Character rendeting.

---
+++ tacticalduel/Character.swift
@@ -0,0 +1,31 @@
+//
+//  Character.swift
+//  tacticalduel
+//
+//  Created by Dmitry Fomin on 14/10/2018.
+//  Copyright © 2018 Dmitry Fomin. All rights reserved.
+//
+
+import SpriteKit
+
+class Character {
+    let node: SKSpriteNode
+    private(set) var q: Int
+    private(set) var r: Int
+    
+    init(q: Int, r: Int, name: String, edge: CGFloat) {
+        node = SKSpriteNode(imageNamed: name)
+        let width = node.frame.width
+        let height = node.frame.height
+        let scale = edge / width
+        node.scale(to: CGSize(width: edge, height: height * scale))
+        
+        self.q = q
+        self.r = r
+    }
+    
+    func setPosition(q: Int, r: Int) {
+        self.q = q
+        self.r = r
+    }
+}
--- tacticalduel/GameScene.swift
+++ tacticalduel/GameScene.swift
@@ -11,17 +11,73 @@ import GameplayKit
 import Hexamap
 
 class GameScene: SKScene {
-    let mapView = HxMapView(radius: 4, edge: 20)
+    var mapView: HxMapView!
+    var characters = [Character]()
+    var selectedCharacter: Character?
 
     override func sceneDidLoad() {
-        for cell in mapView.map.cells.filter({ abs($0.q + $0.r) <= mapView.map.radius }) {
-            let (_, points) = mapView.point(for: cell)
-            var shapePoints = points.map { CGPoint(x: self.frame.width / 2 + $0.x, y: self.frame.height / 2 - $0.y) }
-            let shape = SKShapeNode(points: &shapePoints, count: shapePoints.count)
-            addChild(shape)
-        }
+        let edge = frame.width / CGFloat(11)
+        mapView = HxMapView(radius: 4, edge: edge, center: CGPoint(x: frame.width / 2, y: frame.height / 2))
+        addChild(mapView.node)
+        
+        self.backgroundColor = .gray
+        
+        characters.append(create(name: "night", q: -3, r: 0, edge: edge))
+        characters.append(create(name: "fire", q: -3, r: 3, edge: edge))
+        characters.append(create(name: "water", q: 3, r: -3, edge: edge))
+        characters.append(create(name: "stone", q: 3, r: 0, edge: edge))
+        characters.append(create(name: "ent", q: 0, r: 3, edge: edge))
+        characters.append(create(name: "weather", q: 0, r: -3, edge: edge))
     }
     
     override func update(_ currentTime: TimeInterval) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let location = touch.location(in: self)
+        let point = viewToMap(point: location)
+        if let cell = mapView.cell(for: point) {
+            let characterAtCell = characters.first(where: { $0.q == cell.q && $0.r == cell.r })
+            if characterAtCell != nil {
+                selectedCharacter = characterAtCell
+            } else if let character = selectedCharacter {
+                character.setPosition(q: cell.q, r: cell.r)
+                let newPosition = mapToView(point: mapView.point(for: cell))
+                let moveAction = SKAction.move(to: newPosition, duration: 0.3)
+                moveAction.timingMode = .easeIn
+                character.node.run(moveAction)
+            }
+        }
+    }
+    
+    private func create(name: String, q: Int, r: Int, edge: CGFloat) -> Character {
+        let character = Character(q: q, r: r, name: name, edge: edge)
+        guard let cell = mapView.map.cells.first(where: { $0.q == q && $0.r == r }) else {
+            assert(false)
+            return character
+        }
+        
+        let position = mapToView(point: mapView.point(for: cell))
+        character.node.position = position
+        
+        addChild(character.node)
+        
+        return character
+    }
+    
+    private func mapToView(point: CGPoint) -> CGPoint {
+        var viewPoint = self.convert(point, from: mapView.node)
+        viewPoint.y = frame.height - viewPoint.y
+        return viewPoint
+    }
+    
+    private func viewToMap(point: CGPoint) -> CGPoint {
+        var point = point
+        point.y = frame.height - point.y
+        return self.convert(point, to: mapView.node)
     }
 }
